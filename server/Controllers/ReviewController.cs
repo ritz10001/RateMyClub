a4e0b6f9df8f600b986d138dacfcd013624e073a
ritz10001 <ritvik11prakash@gmail.com>
2025-01-30 22:10:59 -0600

    User checks and specific user endpoint access. For eg. a user who creates a review can only update THAT review

--- server/Controllers/ReviewController.cs
+++ server/Controllers/ReviewController.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Security.Claims;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RateMyCollegeClub.Data;
@@ -35,18 +37,37 @@ public class ReviewController : ControllerBase {
     }
 
     [HttpPost]
+    [Authorize]
     public async Task<ActionResult<CreateReviewDTO>> CreateReview(CreateReviewDTO createReviewDTO) {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("HERE IS THE USER ID");
+        Console.WriteLine(userId);
+        if (userId == null) {
+            return Unauthorized();
+        }
+
         var review = _mapper.Map<Review>(createReviewDTO);
+        review.UserId = userId;
+
         await _reviewsRepository.AddAsync(review);
         return CreatedAtAction("GetReview", new {id = review.Id}, review);
     }
 
     [HttpDelete("{id}")]
+    [Authorize]
     public async Task<IActionResult> DeleteReview(int id){
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if(userId == null){
+            return Unauthorized();
+        }
         var review = await _reviewsRepository.GetAsync(id);
 
         if(review is null){
             return NotFound();
+        }
+        if(review.UserId != userId){
+            return Forbid();
         }
 
         await _reviewsRepository.DeleteAsync(id);
@@ -55,11 +76,19 @@ public class ReviewController : ControllerBase {
     }
 
     [HttpPut("{id}")]
+    [Authorize]
     public async Task<IActionResult> UpdateReview(int id, UpdateReviewDTO updateReviewDTO){
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if(userId == null){
+            return Unauthorized();
+        }
         var review = await _reviewsRepository.GetAsync(id);
 
         if(review is null){
             return NotFound();
+        }
+        if(review.UserId != userId){
+            return Forbid();
         }
 
         _mapper.Map(updateReviewDTO, review);
---
+++ server/Migrations/20250131030717_AddUserIdToReviewModel.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToReviewModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ab7694b7-413b-498c-9032-cee07c923549");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d426a2d7-0112-4960-8266-bda5267c87d3");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Reviews",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "5418773b-c2f4-4183-9fb8-5319f83a04cc", null, "Administrator", "ADMINISTRATOR" },
+                    { "baf37caf-c141-474b-ae4c-56ba85fab8ca", null, "User", "USER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Clubs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 31, 3, 7, 17, 243, DateTimeKind.Utc).AddTicks(9519));
+
+            migrationBuilder.UpdateData(
+                table: "Clubs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 31, 3, 7, 17, 244, DateTimeKind.Utc).AddTicks(1272));
+
+            migrationBuilder.UpdateData(
+                table: "Clubs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 31, 3, 7, 17, 244, DateTimeKind.Utc).AddTicks(1278));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2025, 1, 31, 3, 7, 17, 243, DateTimeKind.Utc).AddTicks(2678), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2025, 1, 31, 3, 7, 17, 243, DateTimeKind.Utc).AddTicks(4744), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2025, 1, 31, 3, 7, 17, 243, DateTimeKind.Utc).AddTicks(4748), "" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5418773b-c2f4-4183-9fb8-5319f83a04cc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "baf37caf-c141-474b-ae4c-56ba85fab8ca");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Reviews",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "ab7694b7-413b-498c-9032-cee07c923549", null, "Administrator", "ADMINISTRATOR" },
+                    { "d426a2d7-0112-4960-8266-bda5267c87d3", null, "User", "USER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Clubs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 26, 18, 57, 27, 413, DateTimeKind.Utc).AddTicks(5546));
+
+            migrationBuilder.UpdateData(
+                table: "Clubs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 26, 18, 57, 27, 413, DateTimeKind.Utc).AddTicks(6579));
+
+            migrationBuilder.UpdateData(
+                table: "Clubs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 26, 18, 57, 27, 413, DateTimeKind.Utc).AddTicks(6583));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2025, 1, 26, 18, 57, 27, 413, DateTimeKind.Utc).AddTicks(1906), null });
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2025, 1, 26, 18, 57, 27, 413, DateTimeKind.Utc).AddTicks(2930), null });
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2025, 1, 26, 18, 57, 27, 413, DateTimeKind.Utc).AddTicks(2932), null });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
