3de388c83783ff895e01097b977fade37774c75a
ritz10001 <ritvik11prakash@gmail.com>
2025-07-16 23:49:56 -0500

    saved reviews wip

--- server/Repository/ReviewsRepository.cs
+++ server/Repository/ReviewsRepository.cs
@@ -1,14 +1,24 @@
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using RateMyCollegeClub.Data;
 using RateMyCollegeClub.Interfaces;
 using RateMyCollegeClub.Models;
 
 namespace RateMyCollegeClub.Repository;
 
-public class ReviewsRepository : GenericRepository<Review>, IReviewsRepository {
+public class ReviewsRepository : GenericRepository<Review>, IReviewsRepository
+{
     private readonly CollegeClubsDbContext _context;
     public ReviewsRepository(CollegeClubsDbContext context) : base(context)
-    {        
+    {
         _context = context;
+    }
+    public async Task<List<Review>> GetReviewsByUserId(string userId)
+    {
+        return await _context.Reviews
+        .Include(r => r.Club)
+        .Include(r => r.Club.University)
+        .Where(r => r.UserId == userId)
+        .ToListAsync();
     }
 }
