9ad96ad5ddab365b086f68d2062366b1fd22e9bc
ritz10001 <ritvik11prakash@gmail.com>
2025-07-20 20:15:46 -0500

    landingpage search + shorthand bookmarks

--- server/Models/Requests/ClubRequests/ClubRequestDTO.cs
+++ server/Models/Requests/ClubRequests/ClubRequestDTO.cs
@@ -7,20 +7,9 @@ public class ClubRequestDTO
 {
     public string FullName { get; set; } = string.Empty;
     public string RequestedBy { get; set; } = string.Empty;
-    [Required]
-    [StringLength(100, MinimumLength = 3, ErrorMessage = "Club name must be between 3-100 characters")]
     public string Name { get; set; } = string.Empty;
-
-    [Required]
-    [StringLength(500, MinimumLength = 20, ErrorMessage = "Description cannot exceed 500 characters")]
     public string Description { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "Please select a category")]
     public int CategoryId { get; set; }
-
-    [Required]
-    [StringLength(100)]
-    public string MeetingLocation { get; set; } = string.Empty;
     public int UniversityId { get; set; }
     public List<int> TagIds { get; set; } = [];
 
