c7c83082eeaa60e97bc19c7580a0fb1e5c010340
ritz10001 <ritvik11prakash@gmail.com>
2025-07-22 00:07:55 -0500

    university and club admin approval/rejects

--- server/Controllers/AdminUniversityController.cs
+++ server/Controllers/AdminUniversityController.cs
@@ -1,4 +1,9 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RateMyCollegeClub.Interfaces;
+using RateMyCollegeClub.Models;
+using RateMyCollegeClub.Models.Universities;
 
 namespace RateMyCollegeClub.Controllers;
 
@@ -7,9 +12,21 @@ namespace RateMyCollegeClub.Controllers;
 
 public class AdminUniversityController : ControllerBase
 {
-    public AdminUniversityController()
+    private readonly IMapper _mapper;
+    private readonly IUniversityRepository _universityRepository;
+    public AdminUniversityController(IMapper mapper, IUniversityRepository universityRepository)
     {
+        _mapper = mapper;
+        _universityRepository = universityRepository;
+    }
 
+    [HttpPost]
+    [Authorize(Roles = "Administrator")]
+    public async Task<ActionResult<CreateUniversityDTO>> CreateUniversity(CreateUniversityDTO createUniversityDTO)
+    {
+        var university = _mapper.Map<University>(createUniversityDTO);
+        await _universityRepository.AddAsync(university);
+        return Ok(university);
     }
 
 }
--- server/Models/Requests/ClubRequests/GetClubRequestsDTO.cs
+++ server/Models/Requests/ClubRequests/GetClubRequestsDTO.cs
@@ -10,7 +10,11 @@ public class GetClubRequestsDTO
     public string FullName { get; set; } = string.Empty;
     public string RequestedBy { get; set; } = string.Empty;
     public string UniversityName { get; set; } = string.Empty;
+    public int UniversityId { get; set; }
     public string Category { get; set; } = string.Empty;
+    public int CategoryId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string ClubLocation { get; set; } = string.Empty;
     public DateTime RequestedAt { get; set; }
     public List<GetTagDTO> Tags { get; set; } = [];
     public string Status { get; set; } 
