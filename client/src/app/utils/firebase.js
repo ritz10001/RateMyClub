c5b5afff557d133cddf50cc34099f4e5c526a89b
ritz10001 <ritvik11prakash@gmail.com>
2025-08-07 16:27:22 -0500

    Fix Firebase config, remove client .gitignore, update packages

--- client/src/app/utils/firebase.js
+++ client/src/app/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEwMFPsY9xPqTKnLUjX0_2pNaK_53HtbM",
-  authDomain: "ratemycollegeclub-1747f.firebaseapp.com",
-  projectId: "ratemycollegeclub-1747f",
-  storageBucket: "ratemycollegeclub-1747f.firebasestorage.app",
-  messagingSenderId: "840578680385",
-  appId: "1:840578680385:web:bd5738c56b0f8e9cf4a8c0",
-  measurementId: "G-ZMXXQFDELY"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
