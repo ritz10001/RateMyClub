e866221722359075849c81bdc4f6aebf6b88451b
ritz10001 <ritvik11prakash@gmail.com>
2025-08-30 20:26:17 -0500

    saved the unsaved file

--- server/Models/Requests/UniversityRequests/EditUniversityRequestDTO.cs
+++ server/Models/Requests/UniversityRequests/EditUniversityRequestDTO.cs
@@ -6,4 +6,5 @@ public class EditUniversityRequestDTO
     public string UniversityName { get; set; } = string.Empty;
     public string Location { get; set; } = string.Empty;
     public string? OfficialWebsite { get; set; } = string.Empty;
+    public string? AdditionalInfo { get; set; } = string.Empty;
 }
